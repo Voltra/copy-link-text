ee4ae97353fd8a11410c8eb950de01fd7c2c52f2
Voltra <ludwig.guerin.98@gmx.fr>
2025-02-06 13:37:45 +0100

    Make the extension translatable

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,16 @@
 import browser from "webextension-polyfill";
 
-const menuId = "copy-link-text__copyLinkText";
+// We save the unique (and extension-scoped) menu ID
+// so that we're able to identify it in event
+// listeners as well as being able to
+// update it later on.
+let menuId = "copy-link-text__copyLinkText" as string | number;
 
-browser.menus.create({
-	id: menuId,
+// We create the menu item as soon as the extension boots
+// that way it's available throughout the lifetime
+// of the browser window.
+menuId = browser.menus.create({
+	id: menuId as string,
 	title: browser.i18n.getMessage("menuTitle"),
 	contexts: ["link"],
 	icons: {
@@ -15,6 +22,31 @@ browser.menus.create({
 	},
 });
 
+// Everytime a menu (item) is shown we update ours
+// so that changes in active/current locale
+// are properly detected.
+browser.menus.onShown.addListener(info => {
+	if (!info.menuIds.includes(menuId)) {
+		return;
+	}
+
+	browser.menus
+		.update(menuId, {
+			title: browser.i18n.getMessage("menuTitle"),
+		})
+		.catch((err: unknown) => {
+			console.error(
+				"[copy-link-text] Failed to update the menu item's title",
+			);
+			console.error("[copy-link-text]", err);
+		});
+});
+
+// When clicking on our menu (item) we want the
+// target link's text to be copied to the
+// user's clipboard. As such we use
+// the browser's Clipboard API
+// instead of the extensions'
 browser.menus.onClicked.addListener(info => {
 	if (info.menuItemId !== menuId || !info.linkText) {
 		return;
